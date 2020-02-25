f797602c6453ee7a437d3d94545b30cbb8408843
andre moratore <cladivino2@gmail.com>
2020-02-24 21:36:23 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/andremoratore/cursomc/domain/Estado.java
+++ src/main/java/com/andremoratore/cursomc/domain/Estado.java
@@ -10,6 +10,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+
+
 
 @Entity
  public class Estado implements Serializable{
@@ -20,7 +24,7 @@ import javax.persistence.OneToMany;
 	private Integer id;
 	private String nome;
  
-	
+	@JsonBackReference
 	@OneToMany(mappedBy = "estado")
 	private List<Cidade> cidades = new ArrayList<>();
 	
